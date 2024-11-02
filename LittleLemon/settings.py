a9696bd440de736a4427a07b75eb174452f71bbe
Dark Vamp <89553338+DarkVamps@users.noreply.github.com>
2024-11-02 21:27:37 +0200

    Added Tables

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,11 +83,10 @@ WSGI_APPLICATION = "LittleLemon.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "littlelemon",
-        "USER": "root",
-        "PASSWORD": "&b825j#YN6rWW2%5SCH%jt^JHH3G*a",
-        "HOST": "localhost",
-        "PORT": "3006",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "read_default_file": os.path.join(BASE_DIR, "my.ini"),
+        },
     }
 }
 
